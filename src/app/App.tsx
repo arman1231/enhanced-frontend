1982dc343907dcf90861633e0a2a6404de5a2119
acherkharov <arman1231@yandex.ru>
2023-10-19 15:17:08 +0300

    add navbar and theme handling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,20 +5,16 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
             <button onClick={toggleTheme}>TOGGLE THEME</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сйате</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
         </div>
     );
 };
