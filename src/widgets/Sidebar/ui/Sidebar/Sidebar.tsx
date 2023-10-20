391ca5b2205bd9d64617195dbcf14711ff730b85
acherkharov <arman1231@yandex.ru>
2023-10-20 17:53:16 +0300

    add i18n

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,10 +2,12 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from "./Sidebar.module.scss";
 import React, {useState} from "react";
 import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
     className?: string;
 }
+
 export const Sidebar = ({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
 
@@ -16,7 +18,8 @@ export const Sidebar = ({className}: SidebarProps) => {
         <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
             <button onClick={onToggle}>toggle</button>
             <div className={cls.switchers}>
-                <ThemeSwitcher />
+                <ThemeSwitcher/>
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
