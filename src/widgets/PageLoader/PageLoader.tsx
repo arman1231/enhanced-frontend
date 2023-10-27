36e25d849c8d4be6e37481e60820c54fbb2b4e9a
acherkharov <arman1231@yandex.ru>
2023-10-27 17:04:13 +0300

    add PageLoader

---
+++ src/widgets/PageLoader/PageLoader.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import './PageLoader.scss'
+import { Loader } from 'shared/ui/Loader/Loader'
+
+interface PageLoaderProps {
+  className?: string
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => {
+  return (
+        <div className={classNames('page__loader', {}, [className])}>
+            <Loader />
+        </div>
+  )
+}
